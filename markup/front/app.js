5086afdde4986f311978eccc7c9c0b1fca075d8c
fureinzzz <kopylovreigver75@mail.ru>
2019-01-15 18:30:08 +0300

    Добавлено уведомление об ошибке

--- markup/front/app.js
+++ markup/front/app.js
@@ -4,3 +4,27 @@ import {rippleEffect} from './js_modules/rippleEffect.js'
 const JSinput = new JSinputImplementation('.bj', {placeholder: '.Rjx2D'})
 const ripple = new rippleEffect('.loginbtn')
 
+
+function errorAlert(wrap, alertblock){
+    const alert = document.querySelector(alertblock)
+    const parent = document.querySelectorAll(wrap)
+
+    Array.prototype.forEach.call(parent, element =>{
+        const value = element.querySelector('input').value
+        
+            if(value.trim() == ''){
+                element.classList.add('_empty')
+                alert.classList.add('AjmAWC')
+            }
+            else{
+                element.classList.remove('_empty')
+                alert.classList.remove('AjmAWC')
+            }
+    })
+}
+
+
+//  Слушатель заглушка
+document.querySelector('.loginbtn').addEventListener('click', function(){
+    errorAlert('.bj', '.RWacdkJs')
+})
