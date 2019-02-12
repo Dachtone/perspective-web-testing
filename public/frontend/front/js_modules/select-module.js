562d174344c6ac954facf71ab8cf69e217fb2909
fureinzzz <kopylovreigver75@mail.ru>
2019-02-12 22:15:14 +0300

    новый костыль :)

--- public/frontend/front/js_modules/select-module.js
+++ public/frontend/front/js_modules/select-module.js
@@ -16,14 +16,22 @@ export class FZselect{
     initTittle(obj){
         const place = obj.querySelector('select-placeholder')
         const content = place.getAttribute('data-this')
-
+        const costyul = createElement('select')
+        const son_costyul = createElement('option')
         const ttle = createElement('div', {class: 'place'})
         const icon = createElement('select-icon')
          ttle.innerHTML = content;
          icon.innerHTML = ' <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7 10l5 5 5-5z"/><path d="M0 0h24v24H0z" fill="none"/></svg>'
 
+        costyul.setAttribute('form','registerForm')
+        costyul.setAttribute('required', '')
+        costyul.setAttribute('name', 'inputType')
+        son_costyul.setAttribute('selected', '')
+        costyul.appendChild(son_costyul)
+
         place.appendChild(ttle)
         place.appendChild(icon)
+        place.appendChild(costyul)
     }
     tamplateSet(){
         Array.prototype.forEach.call(this.obj, item =>{
@@ -38,6 +46,7 @@ export class FZselect{
         wrap.style = `top: ${element.clientHeight}px`
     }
     addListener(element){
+      
         const html = document.querySelector('html')
         const wrapper = element
         const item = element.querySelectorAll('select-item')
@@ -75,9 +84,11 @@ export class FZselect{
         this.initTrue(data.obj)
     }
     initChange(data){
+        const select = data.obj.querySelector('option')
         const place = data.obj.querySelector('.place')
         const data_variant = data.item.textContent
-        
+        select.setAttribute('value', data_variant)
+        select.textContent = data_variant
         place.textContent = data_variant
     }
 }
