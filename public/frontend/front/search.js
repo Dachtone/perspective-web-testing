327e65b5fbf118f2ad3b76c2524eb8626687938d
fureinzzz <kopylovreigver75@mail.ru>
2019-02-18 21:57:50 +0300

    filter update

--- public/frontend/front/search.js
+++ public/frontend/front/search.js
@@ -1,3 +1,13 @@
+'use strict'
+
+Object.size = (obj)=>{
+    let size = 0, key;
+    for(key in obj){
+        if(obj.hasOwnProperty(key)) size++;
+    }
+    return size
+}
+
 const button = document.querySelector('.filter-open')
 const modal = document.querySelector('.filter-modal')
 const search = document.querySelector('.filter-modal button')
@@ -22,22 +32,67 @@ function addListener(){
     })
 }
 function collectDatafromFilter(){
+    let data = []
+    let radio
     const parent = document.querySelector('.filter-modal')
 
     const tittle = parent.querySelector('[name="headline"]')
     const subject = parent.querySelector('[name="subject"]')
-    const theme = parent.querySelector('[name="topic"]')
-    const semester = parent.querySelector('[name="semester"]')
-    const author = parent.querySelector('[name="author"]')
+    const theme = parent.querySelector('[name="topic"] select')
+    const semester = parent.querySelector('[name="semester"] select')
+    const author = parent.querySelector('[name="author"] select')
     const completed = parent.querySelectorAll('input[type="radio"]')
 
     Array.prototype.forEach.call(completed, item =>{
         if(item.checked){
-            console.log(item.value)
+            radio = item.value
         }
     })
+    
+    data.push({
+        'headline': tittle.value.trim(),
+        'subject': subject.value.trim(),
+        'topic': theme.value,
+        'semester': semester.value,
+        'author': author.value,
+        'completed': radio === undefined ? '' : radio
+    })
+    clear(data)
+}
+function clear(value){
+    const item = value[0]
+
+    for(let key in item){
+        if(item[key] == ''){
+            delete item[key]
+        }
+    }
+    addMask(item)
 }
 
+function addMask(data){
+    let result = ''
+    let elementPosition = 0
+    const length = Object.size(data)
+
+    for(let key in data){
+        elementPosition++;
+        if(elementPosition !== length){
+            result += `${key}=${data[key]}&`
+        }else{
+            result += `${key}=${data[key]}`
+        }
+    }
+
+    sendAjax_filter(result)
+}
+function sendAjax_filter(data){
+    const http = new XMLHttpRequest()
+
+    http.open('GET', '/test')
+    http.send(data)
+    window.location.replace(`/tests?${data}`)
+}
 
 function setPosition(){
     const offset = button.offsetTop
