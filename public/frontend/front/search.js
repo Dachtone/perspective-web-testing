9be758ed675685c4576f3b1b37268fe537a9a03d
fureinzzz <kopylovreigver75@mail.ru>
2019-02-18 20:35:11 +0300

    update create_test page

--- public/frontend/front/search.js
+++ public/frontend/front/search.js
@@ -1,5 +1,8 @@
 const button = document.querySelector('.filter-open')
 const modal = document.querySelector('.filter-modal')
+const search = document.querySelector('.filter-modal button')
+
+
 function addListener(){
     document.addEventListener('mousedown', ({target})=>{
        if(modal.getAttribute('hidden-modal') == null){
@@ -11,7 +14,31 @@ function addListener(){
     button.addEventListener('click', ()=> {
         modal.toggleAttribute('hidden-modal')
     })
+    search.addEventListener('click', (e)=> {
+        e.preventDefault()
+
+        collectDatafromFilter()
+
+    })
 }
+function collectDatafromFilter(){
+    const parent = document.querySelector('.filter-modal')
+
+    const tittle = parent.querySelector('[name="headline"]')
+    const subject = parent.querySelector('[name="subject"]')
+    const theme = parent.querySelector('[name="topic"]')
+    const semester = parent.querySelector('[name="semester"]')
+    const author = parent.querySelector('[name="author"]')
+    const completed = parent.querySelectorAll('input[type="radio"]')
+
+    Array.prototype.forEach.call(completed, item =>{
+        if(item.checked){
+            console.log(item.value)
+        }
+    })
+}
+
+
 function setPosition(){
     const offset = button.offsetTop
     const height = button.offsetHeight
