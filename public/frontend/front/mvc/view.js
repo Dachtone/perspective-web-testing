bf530c5fb3f1d37f3511103464a629528ea5c7fd
fureinzzz <kopylovreigver75@mail.ru>
2019-02-27 22:29:50 +0300

    add crtl-open-menu function

--- public/frontend/front/mvc/view.js
+++ public/frontend/front/mvc/view.js
@@ -16,8 +16,22 @@ export class View{
         this.menu.setAttribute('hidden', '')
         this.overlay.setAttribute('hidden', '')
     }
+    _menu_switcher(){
+        if(this.menu.getAttribute('ctrl-state') == 'false'){
+            this.menu.setAttribute('ctrl-state', 'true')
+
+            this._openmenu()
+        }else if(this.menu.getAttribute('ctrl-state') == 'true'){
+            this.menu.setAttribute('ctrl-state', 'false')
+
+            this._closemenu()
+        }
+    }
     _listeners(){
        this.menu_btn.addEventListener('click', () =>{ this._openmenu()})
        this.overlay.addEventListener('mousedown', () =>{ this._closemenu()})
+       document.addEventListener('keyup', (e)=>{
+           e.keyCode == 17 ? this._menu_switcher() : 0
+       })
     }
 }
